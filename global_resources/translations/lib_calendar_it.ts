2c581e616b8053155f94701f2af7b903e16dabba
filianhouse <filianoti@gmail.com>
2014-12-12 16:57:47 +0100

    Update lib_calendar_it.ts

--- global_resources/translations/lib_calendar_it.ts
+++ global_resources/translations/lib_calendar_it.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="177"/>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="477"/>
-        <source>View range</source>
+        <source>Vista</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -14,17 +14,17 @@
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="431"/>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="435"/>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="439"/>
-        <source>d MMM yyyy</source>
+        <source>g MMM aaaa</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="434"/>
-        <source>d MMM</source>
+        <source>g MMM</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/calendar_navbar.cpp" line="438"/>
-        <source>d</source>
+        <source>g</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -32,12 +32,12 @@
     <name>Calendar::Internal::BasicItemEditionDialog</name>
     <message>
         <location filename="../../libs/calendar/modelanditem/basic_item_edition_dialog.ui" line="14"/>
-        <source>Calendar item editor</source>
+        <source>modifica Calendario</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/basic_item_edition_dialog.ui" line="45"/>
-        <source>Appointment editor</source>
+        <source>modifica Appuntamenti</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -45,12 +45,12 @@
     <name>Calendar::Internal::DayRangeBody</name>
     <message>
         <location filename="../../libs/calendar/dayview/dayrangebody.cpp" line="678"/>
-        <source>modify</source>
+        <source>modifica</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/dayview/dayrangebody.cpp" line="680"/>
-        <source>remove</source>
+        <source>rimuovi</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -58,12 +58,12 @@
     <name>Calendar::Internal::DayRangeHeader</name>
     <message>
         <location filename="../../libs/calendar/dayview/dayrangeheader.cpp" line="392"/>
-        <source>modify</source>
+        <source>modifica</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/dayview/dayrangeheader.cpp" line="394"/>
-        <source>remove</source>
+        <source>rimuovi</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -71,7 +71,7 @@
     <name>Calendar::Internal::DayWidget</name>
     <message>
         <location filename="../../libs/calendar/dayview/day_widget.cpp" line="75"/>
-        <source>(untitled)</source>
+        <source>(senza titolo)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -79,7 +79,7 @@
     <name>Calendar::Internal::HourRangeWidget</name>
     <message>
         <location filename="../../libs/calendar/dayview/hour_range_widget.cpp" line="101"/>
-        <source>(untitled)</source>
+        <source>(senza titolo)</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -92,47 +92,47 @@
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="40"/>
-        <source>Date, time and label</source>
+        <source>Data, ora ed etichetta</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="49"/>
-        <source>is private</source>
+        <source>è privato</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="58"/>
-        <source>Protect with password:</source>
+        <source>Protetto con password:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="92"/>
-        <source>Label:</source>
+        <source>etichetta:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="102"/>
-        <source>show as busy</source>
+        <source>mostra occupato</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="127"/>
-        <source>Duration:</source>
+        <source>Durata:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="143"/>
-        <source>Status:</source>
+        <source>Stato:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="166"/>
-        <source>Start:</source>
+        <source>Inizio:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="179"/>
-        <source>End:</source>
+        <source>Fine:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -142,42 +142,42 @@
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="241"/>
-        <source>General information</source>
+        <source>Informazioni generali</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="247"/>
-        <source>Type:</source>
+        <source>Tipo:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="257"/>
-        <source>Location:</source>
+        <source>Luogo:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="267"/>
-        <source>Calendar:</source>
+        <source>Calendario:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="281"/>
-        <source>Description</source>
+        <source>Descrizione</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="293"/>
-        <source>Extended description</source>
+        <source>Descrizione estesa</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="314"/>
-        <source>Full information:</source>
+        <source>Tutte le informazioni:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/modelanditem/item_editor_widget.ui" line="357"/>
-        <source>Icon</source>
+        <source>Icona</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -185,12 +185,12 @@
     <name>Calendar::Internal::MonthBody</name>
     <message>
         <location filename="../../libs/calendar/monthview/month_view.cpp" line="114"/>
-        <source>d MMM</source>
+        <source>g MMM</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../libs/calendar/monthview/month_view.cpp" line="116"/>
-        <source>d</source>
+        <source>g</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -198,7 +198,7 @@
     <name>Calendar::MonthDayWidget</name>
     <message>
         <location filename="../../libs/calendar/monthview/month_day_widget.cpp" line="91"/>
-        <source>see others</source>
+        <source>vedi altri</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
